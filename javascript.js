8dc90a19a57279b77fb37f89cbaed558c391e3be
Kris Chambers <krischambers86@pm.me>
2022-10-07 09:36:54 -0400

    Add hover to change square color

--- javascript.js
+++ javascript.js
@@ -3,6 +3,15 @@ board.style.gridTemplateColumns = "repeat(16, 1fr)";
 board.style.gridTemplateRows = "repeat(16, 1fr)";
   for (let i = 0; i < 256; i++) {
     let square = document.createElement("div");
+    square.className = "square",
     square.style.backgroundColor = "blue";
     board.insertAdjacentElement("beforeend", square);
+    square.addEventListener("mouseover", () => {
+      square.style.backgroundColor = "white";
+    });
   }
+
+let button = document.querySelector(".button")
+button.addEventListener("click", () => {
+  let size= prompt("Choose grid size");
+})
