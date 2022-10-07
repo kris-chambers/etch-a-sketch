51e6531700617c53f48f5ca1ae378a246338a385
Kris Chambers <krischambers86@pm.me>
2022-10-07 18:09:20 -0400

    Add JS to functions, add grid size button functionality

--- javascript.js
+++ javascript.js
@@ -14,4 +14,26 @@ board.style.gridTemplateRows = "repeat(16, 1fr)";
 let button = document.querySelector(".button")
 button.addEventListener("click", () => {
   let size= prompt("Choose grid size");
-})
+  grid(size);
+  drawBoard(size);
+});
+
+function grid(size) {
+  let board = document.querySelector(".board");
+  board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    
+};
+
+function drawBoard(size) {
+  for (let i = 0; i < size * size; i++) {
+    let board = document.querySelector(".board");
+    let square = document.createElement("div");
+    square.className = "square";
+    square.style.backgroundColor = "blue";
+    board.insertAdjacentElement("beforeend", square);
+    square.addEventListener("mouseover", () => {
+      square.style.backgroundColor = "white";
+    })
+  }
+};
