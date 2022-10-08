9068cbda919a608ce4e5b69062f72200694d014d
Kris Chambers <krischambers86@pm.me>
2022-10-07 22:29:26 -0400

    Finish reset button functionality...finally

--- javascript.js
+++ javascript.js
@@ -1,35 +1,26 @@
 let board = document.querySelector(".board");
 let button = document.querySelector(".button");
 let button2 = document.querySelector(".button2");
-
-// board.style.gridTemplateColumns = "repeat(16, 1fr)";
-// board.style.gridTemplateRows = "repeat(16, 1fr)";
-//   for (let i = 0; i < 256; i++) {
-//     let block = document.createElement("div");
-//     block.className = "square",
-//     block.style.backgroundColor = "blue";
-//     board.insertAdjacentElement("beforeend", block);
-//     block.addEventListener("mouseover", () => {
-//       block.style.backgroundColor = "white";
-//     });
-//   }
-
+let bottom = document.querySelector(".bottom");
 
 button.addEventListener("click", () => {
+// Grid size button to draw board  
   let size= prompt("Choose grid size");
   grid(size);
   drawBoard(size);
 });
 
 button2.addEventListener("click", () => {
-  resetBoard();
+// Reset button to reset board  
+  board.innerHTML = '';
+  button.click();
 });
 
 // function to recolor squares with subsequent passes
 
 function grid(size) {
 // draw the grid given x size  
-  // let board = document.querySelector(".board");
+  let board = document.querySelector(".board");
   board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
   board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
     
@@ -41,15 +32,10 @@ function drawBoard(size) {
     let square = document.createElement("div");
     square.className = "square";
     square.setAttribute("id", "square");
-    square.setAttribute("style", "background-color:blue");
+    square.setAttribute("style", "background-color:white");
     board.insertAdjacentElement("beforeend", square);
     square.addEventListener("mouseover", () => {
-      square.style.backgroundColor = "white";
+      square.style.backgroundColor = "black";
     })
   }
 };
-
-function resetBoard() {
-  // reset board colors
-    document.getElementById(square).style.backgroundColor = "blue";
-}
