990503ed57ab69b8e2b20cf7734e7e2486c0e132
Kris Chambers <103455140+kris-chambers@users.noreply.github.com>
2025-02-10 21:02:12 -0500

    Can change grid size

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
-let gridSize = 25;
-
 const container = document.querySelector(".container");
+const gridColumn = document.querySelectorAll(".column");
+const btn = document.querySelector(".button");
+
 
 function drawGrid(gridSize) {
   for (let i = 0; i < gridSize; i++) {
@@ -15,17 +16,34 @@ function drawGrid(gridSize) {
   }
 }
 
-drawGrid(gridSize);
+function clearGrid() {
+  container.innerHTML = "";
+};
+  
 
-const gridRow = document.querySelectorAll(".row");
+btn.addEventListener('click', () => {
+  const prompt = window.prompt("Please enter a number from 1-100")
+  gridSize = Number(prompt);
+  if ((Number(prompt) > 0) && (Number(prompt < 101))) {
+    clearGrid();
+    drawGrid(gridSize);
+    changeColor();
+  } else {
+    alert("You did not enter a number from 1-100.");
+  }
+})
 
-for (const square of gridRow) {
-  square.addEventListener('mouseover', () => {
-    const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
-    square.style.backgroundColor = randomColor;
-    console.log("change color");
-    })
+drawGrid(16);
+
+function changeColor() {
+  const gridRow = document.querySelectorAll(".row");
+  for (const square of gridRow) {
+    square.addEventListener('mouseover', () => {
+      const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+      square.style.backgroundColor = randomColor;
+      console.log("change color");
+      })
+  }
 }
 
-
-
+changeColor();
